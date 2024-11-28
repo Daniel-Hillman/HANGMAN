c6b5d2760217cb3c0d8b941b57581ae2f9031aa6
Daniel <danielhillman94@hotmail.co.uk>
2024-11-28 00:44:21 +0000

    changes done to milestone 2 and 3 functiuons added

--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,12 @@
 import random
 
 word_list = ["grape", "apple", "banana", "guava", "mango"]
-word = random.choice(word_list)  
-print(word)
+word = random.choice(word_list)
+
+guess = input("Enter a single letter: ")
+if len(guess) == 1:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+
 
